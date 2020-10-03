f0cbeaa55ce3f59148ec3b75b8e124ef4523a1fd
2734501754 <70470063+2734501754@users.noreply.github.com>
2020-10-03 18:07:07 +0800

    哈哈哈

--- ConsoleApplication1/没啥21.cpp
+++ ConsoleApplication1/没啥21.cpp
@@ -1,10 +1,95 @@
 #include<iostream>
+const int ArSize = 16;
 using namespace std;
-int main()
+int miiain()
 {
-	int a;
-	cin >> a;
-	for (int i = a; i; i--)
-		cout << "i= "<<i<<endl;
+	long long factorials[ArSize];
+	factorials[1] = factorials[0] = 1LL;
+	for (int i = 2; i < ArSize; i++)
+		factorials[i] = i * factorials[i - 1];
+	for (int i = 0; i < ArSize; i++)
+		cout << i << "i= " << factorials[i] << endl;
+	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
 
 }
