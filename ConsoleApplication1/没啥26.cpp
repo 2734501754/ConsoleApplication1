5712bd60e963da4eab39a9516e850f762eb2d8cf
2734501754 <70470063+2734501754@users.noreply.github.com>
2020-10-25 17:25:21 +0800

    额啊

---
+++ ConsoleApplication1/没啥26.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int c;
+	c=( 17, 240);
+	cout << c<<endl;
+	string a = "123", b = "abc"; 
+	cout << (a < b)<<endl;
+	cout << ('a' < 'b')<<endl;
+	int x = 1,y= 3;
+	cout << ((x + 3) > (y - 2))<<endl;
+
+}
